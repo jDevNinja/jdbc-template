27260a2a55714eb16b716de04b398b63e4f523d7
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2023-11-16 22:07:10 +0700

    4. add context initialization and get jdbc-template bean

--- src/main/java/org/element/onex/Main.java
+++ src/main/java/org/element/onex/Main.java
@@ -1,7 +1,12 @@
 package org.element.onex;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
+  public static void main(String[] args) {
+    ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+    JdbcTemplate jdbcTemplate = context.getBean("jdbcTemplate", JdbcTemplate.class);
+  }
 }
